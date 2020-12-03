17dcd668a8562b9e13867ed41605bc87ba66b83f
Veli Bacik <veli.bacik@ibtech.com.tr>
2020-12-04 01:51:59 +0300

    last updated

--- lib/features/maps/view/poly_line_view.dart
+++ lib/features/maps/view/poly_line_view.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fluttermapsadvance/features/maps/cubit/google_maps/google_maps_state.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../../../core/extension/context_extension.dart';
 import '../../../core/network/network_manager.dart';
 import '../../_component/card/coordinate_card/coordinate_card.dart';
 import '../cubit/google_maps/google_maps_cubit.dart';
+import '../cubit/google_maps/google_maps_state.dart';
 import '../cubit/points/points_cubit.dart';
 import '../model/coordinate.dart';
 import '../service/IMapService.dart';
